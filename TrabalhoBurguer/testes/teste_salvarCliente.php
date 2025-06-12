2fc8e0d00b5b553fcd164a5b8d741d16def30c7b
Erick20082705 <erikpedroso2705@gmail.com>
2025-06-12 09:17:27 -0300

    Update teste_salvarCliente.php

--- TrabalhoBurguer/testes/teste_salvarCliente.php
+++ TrabalhoBurguer/testes/teste_salvarCliente.php
@@ -1,8 +1,10 @@
 <?php
-require_once "../conexao.php"
-require_once "./funcoes_haburgueria.php"
+require_once "../conexao.php";
+require_once "./funcoes_haburgueria.php";
 $nome="Carlos";
 $endereco="rua2";
+$telefone="123456789";
+$senha="123";
 
 salvarCliente($conexao, $nome, $endereco, $telefone, $senha)
 ?>
