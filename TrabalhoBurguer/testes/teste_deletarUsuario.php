bc6e1ec351b81530976690a6edf80cc17703dc9a
Erick20082705 <erikpedroso2705@gmail.com>
2025-06-23 14:15:05 -0300

    Update teste_deletarUsuario.php

--- TrabalhoBurguer/testes/teste_deletarUsuario.php
+++ TrabalhoBurguer/testes/teste_deletarUsuario.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "../funcoes/funcoes_hamburgeria.php";
+    require_once "../funcoes/funcoes_hamburgueria.php";
     require_once "../conexao.php";
 
     deletarUsuario($conexao, 1);
