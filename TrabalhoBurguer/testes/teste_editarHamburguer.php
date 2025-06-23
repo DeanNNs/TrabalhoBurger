fd8804a95ae1e298afe69d8a29a4689edb63258d
Matheus <matheussilva321647@gmail.com>
2025-06-23 13:55:26 -0300

    fix and create editar combo

--- TrabalhoBurguer/testes/teste_editarHamburguer.php
+++ TrabalhoBurguer/testes/teste_editarHamburguer.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once "../conexao.php";
-require_once "../funcoes/funcoes_hamburgeria.php";
+require_once "../funcoes/funcoes_hamburgueria.php";
 
 $idhamburguer = 1;
 $nome = "Hamburguer de queijo";
 $preco = "12.00";
 $descricao = "Pao e queijo";
 
-editarHamburger($conexao, $nome, $preco, $descricao, $idhamburguer);
+editarHamburguer($conexao, $nome, $preco, $descricao, $idhamburguer);
 ?>
