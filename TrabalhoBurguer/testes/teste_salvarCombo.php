0cd82d9498f8c6cc0f6ec632d4b6eea76e8654d6
Erick20082705 <erikpedroso2705@gmail.com>
2025-06-26 09:00:01 -0300

    Update teste_salvarCombo.php

--- TrabalhoBurguer/testes/teste_salvarCombo.php
+++ TrabalhoBurguer/testes/teste_salvarCombo.php
@@ -5,8 +5,8 @@ require_once "../funcoes/funcoes_hamburgueria.php";
 $nome= "Hamburguer de cheddar duplo ao molho de mingau, coca cola de 2l, batata frita";
 $preco = "15.00";
 $descricao = "Hamburguer de cheddar duplo ao molho de mingau";
-$bebida_idbebida = 2;
-$adicional_idadicional = 2;
+$bebida_idbebida = 1;
+$adicional_idadicional = 1;
 $hamburguer_idhamburguer = 1;
 
 salvarCombo($conexao, $nome, $preco, $descricao, $bebida_idbebida, $adicional_idadicional, $hamburguer_idhamburguer);
