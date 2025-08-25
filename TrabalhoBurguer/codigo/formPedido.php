eba9544acba52ebc0ddf2349ec03b15f7e9cb043
DeanNNs <168701181+DeanNNs@users.noreply.github.com>
2025-08-25 13:31:27 -0300

    update

---
+++ TrabalhoBurguer/codigo/formPedido.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar Produtos</title>
+</head>
+<body>
+    <form action="salvarPedido.php" method="get">
+    Data: <br>
+    <input type="text" name="nome"> <br><br>
+    Endereço: <br>
+    <input type="text" name="preco">
+    <br><br>
+    Telefone: <br>
+    <input type="text" name="descricao">
+    <br><br>
+    Tipo do produto: <br>
+    <input type="text" name="tipo">
+    <br><br>
+    <input type="submit" value="Cadastrar"> 
+
+    </form>
+</body>
+</html>
