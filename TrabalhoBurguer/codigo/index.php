418b8b1e1c1a78ed324f703e471b06717dfe5c8e
DeanNNs <168701181+DeanNNs@users.noreply.github.com>
2025-09-08 13:49:39 -0300

    creates

---
+++ TrabalhoBurguer/codigo/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Allapolo's Burger</h1>
+
+    <br><br><br><br><br><br>
+    <div class="d-grid gap-2 col-6 mx-auto">
+
+
+    <br>
+        <a href="formUsuario.php"> <img src="../imgs/user.png" alt="user"> </a> <br><br>
+        <a target="conteudo" class="btn btn-primary" href="./funcionario.php">Cadastrar um funcionário</a><br><br>
+        <a target="conteudo" class="btn btn-primary" href="./emprestimo.php">Cadastrar um empréstimo</a> <br><br>
+        <a target="conteudo" class="btn btn-primary" href="./usuario.php">Cadastrar Usuario</a><br><br>
+    </div>
+    <br><br><br>
+
+
+</body>
+</html>
