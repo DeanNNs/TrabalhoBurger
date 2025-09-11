9a2e5c4221bcbee364bb931a317e997b4abfc79a
DeanNNs <168701181+DeanNNs@users.noreply.github.com>
2025-09-11 10:26:54 -0300

    fix banco

--- TrabalhoBurguer/testes/teste_salvarProduto.php
+++ TrabalhoBurguer/testes/teste_salvarProduto.php
@@ -2,9 +2,9 @@
     require_once "../funcoes/funcoes_hamburgueria.php";
     require_once "../conexao.php";
 
-    $nome="Suco de tamarindo";
-    $preco=10.50;
-    $descricao="Garrafa de 1,5 litro de suco de maracujá";
+    $nome="Burgão monstro";
+    $preco=25.50;
+    $descricao="Hamburguer";
     $tipo="C";
     salvarProduto($conexao, $nome, $preco, $descricao, $tipo);
 
