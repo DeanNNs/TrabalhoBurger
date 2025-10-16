0b88383ae9c28d1b19eae24bf3a27e1e33d4187c
Erick20082705 <erikpedroso2705@gmail.com>
2025-10-16 16:47:52 -0300

    estilo de coisaxx

--- TrabalhoBurguer/index.php
+++ TrabalhoBurguer/index.php
@@ -9,14 +9,17 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css"></link>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Honk&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./css/style.css"></link>
 </head>
 <body>
-    <h1>Allapolo's Burger</h1>
+    <h1 class="h1">Allapolo's Burger</h1>
 
-    <iframe name="principal" src="/codigo/telaCompra.php" width="1000" height="600" frameborder="1"></iframe>
-    <iframe src="/codigo/categoriaProduto.php" width="1000" height="600" frameborder="1"></iframe>
+    <iframe name="principal" src="/codigo/telaCompra.php" width="1000" height="600" frameborder="0"></iframe>
+    <iframe src="/codigo/categoriaProduto.php" width="1000" height="600" frameborder="0"></iframe>
 
     <a href="/codigo/carrinho.php"> <img src="/imgs/carrinho.png" class="carrinho" width="50" height="50"> </a>
 
@@ -26,13 +29,6 @@ session_start();
     } else {
        echo '<a href="/codigo/telaLogin.php"> <img src="/imgs/user.png" class="user" width="50" height="50"> </a>';    
     }
-
-    
-
 ?>
-
-
-
-
 </body>
 </html>
