6bb6ff79c2617acb2ea2bfecb6f6a708361f0790
Erick20082705 <erikpedroso2705@gmail.com>
2025-10-16 16:06:59 -0300

    coisas

--- TrabalhoBurguer/index.php
+++ TrabalhoBurguer/index.php
@@ -9,6 +9,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css"></link>
 </head>
 <body>
     <h1>Allapolo's Burger</h1>
