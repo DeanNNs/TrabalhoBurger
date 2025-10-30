c955fd627524fd08f11dc804cba5dc9c6c3dd2b2
DeanNNs <168701181+DeanNNs@users.noreply.github.com>
2025-10-30 14:37:37 -0300

    creates

---
+++ TrabalhoBurguer/telaAdministrador.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Modo Administrador</h1> <br><br><br>
+    <a href="./codigo/formProduto.html">Adicionar um novo produto</a>
+</body>
+</html>
