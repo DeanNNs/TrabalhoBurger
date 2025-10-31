576b4b90af68761f5395b5179afce2e857081862
Dean <168701181+DeanNNs@users.noreply.github.com>
2025-10-31 15:15:55 -0300

    Create

--- TrabalhoBurguer/telaAdministrador.php
+++ TrabalhoBurguer/telaAdministrador.php
@@ -4,13 +4,17 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Modo Administrador</h1> <br><br><br>
     <a href="./codigo/formProduto.html">Adicionar um novo produto</a>
+    <a href="./codigo/listarUsuario.php">Listar Usuarios</a>
 </body>
+
 </html>
