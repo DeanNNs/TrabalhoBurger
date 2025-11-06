c86a7ae0fcb8a59b45bf7f5936184c327dcf6811
DeanNNs <168701181+DeanNNs@users.noreply.github.com>
2025-11-06 14:23:05 -0300

    create and fix

--- TrabalhoBurguer/codigo/listarHamburguer.php
+++ TrabalhoBurguer/codigo/listarHamburguer.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <body>
      <?php
     require_once "../conexao.php";
@@ -43,4 +36,3 @@
 
     <button><a style="text-decoration: none" href="telaCompra.php">Voltar</a></button>
 </body>
-</html>
--- TrabalhoBurguer/codigo/salvarPedido.php
+++ TrabalhoBurguer/codigo/salvarPedido.php
@@ -3,8 +3,9 @@
     require_once "../conexao.php";
     require_once "../funcoes/funcoes_hamburgueria.php";
 
-    $data = $_GET['data'];
-    $endereco = $_GET['endereco'];
+
+    $data = $_POST['data'];
+    $endereco = $_POST['endereco'];
     $valor_total = $_GET['total'];
     $idusuario = $_SESSION['idusuario'];
     
