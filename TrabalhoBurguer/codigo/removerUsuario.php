f9fee48fbfb8555b6844977c8fedf14af5df29b8
DeanNNs <168701181+DeanNNs@users.noreply.github.com>
2025-11-06 08:48:02 -0300

    create css

--- TrabalhoBurguer/codigo/removerUsuario.php
+++ TrabalhoBurguer/codigo/removerUsuario.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-    <?php
-    require_once "../conexao.php";
-    require_once "../funcoes/funcoes_hamburgueria.php";
+<?php
+  require_once "../conexao.php";
+  require_once "../funcoes/funcoes_hamburgueria.php";
 
-    $idusuario = $_GET['id'];
+  $idusuario = $_GET['id'];
 
-    deletarUsuario($conexao, $idusuario);
+  deletarUsuario($conexao, $idusuario);
 
-header("Location: listarUsuario.php");
-    ?>
-</body>
-</html>
+  header("Location: listarUsuario.php");
+?>
