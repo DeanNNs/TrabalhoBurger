a4807d7552748040ae4ed169753544c40f4a7ea7
Matheus <matheussilva321647@gmail.com>
2026-01-04 15:47:11 -0300

    s

--- conexao.php
+++ conexao.php
@@ -1,7 +1,14 @@
 <?php
-$servidor = 'db';
-$usuario = 'root';
-$password = '123';
-$banco = 'trabalho_bd';
+// O getenv busca as variáveis da imagem 325. Se não existirem (seu PC), usa o padrão local.
+$servidor = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
+$usuario  = getenv('MYSQLUSER') ?: 'root';
+$senha    = getenv('MYSQLPASSWORD') ?: 'SCeAFBVMbpnExENKwKXybUVcgLbsiEAk';
+$banco    = getenv('MYSQLDATABASE') ?: 'railway';
+$porta    = getenv('MYSQLPORT') ?: '3306';
 
-$conexao = mysqli_connect($servidor, $usuario, $password, $banco);
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $porta);
+
+if (!$conexao) {
+    die("Erro na conexão: " . mysqli_connect_error());
+}
+?>
